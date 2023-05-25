6eaddd529d15bf6fee4a27c34afda70d0e073a91
Jeremy Cheng <81793294+Jeremy-Cheng-stack@users.noreply.github.com>
2023-05-25 16:55:49 -0400

    Added setup for DB, routers

--- back-end/index.js
+++ back-end/index.js
@@ -3,14 +3,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const example_router_1 = require("./routers/example_router");
 dotenv_1.default.config();
-const app = (0, express_1.default)();
-const port = process.env.PORT;
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server is hello');
+exports.app = (0, express_1.default)();
+exports.app.use(body_parser_1.default.json());
+let dburl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.ha35gkz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose_1.default.connect(dburl)
+    .then(() => {
+    console.log('Connected to the database ');
+})
+    .catch((err) => {
+    console.error(`Error connecting to the database. n${err}`);
 });
-app.listen(port, () => {
+exports.app.use("/api/example", example_router_1.exampleRouter);
+const port = process.env.PORT;
+exports.app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
--- back-end/index.ts
+++ back-end/index.ts
@@ -1,15 +1,28 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import { exampleRouter } from './routers/example_router';
+
 
 dotenv.config();
 
-const app: Express = express();
+export const app: Express = express();
+
+app.use(bodyParser.json());
+
+let dburl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.ha35gkz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(dburl)
+  .then( () => {
+      console.log('Connected to the database ');
+  })
+  .catch( (err) => {
+      console.error(`Error connecting to the database. n${err}`);
+  })
+
+app.use("/api/example", exampleRouter);
+
 const port = process.env.PORT;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server is hello');
-});
-
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
